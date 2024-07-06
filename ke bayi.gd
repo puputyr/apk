e34a469db766f88855a9a26a05eea1587ba02bac
Puputtt <puputsn9@gmail.com>
2024-07-06 14:50:12 +0700

    bayi 9-12 on going

---
+++ ke bayi.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var is_clicked: bool = false
+
+
+func _input(event):
+	if (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT) or event is InputEventScreenTouch and event.pressed :
+		if is_clicked == true:
+			get_tree().change_scene_to_file("res://bayi9-12.tscn")
+
+func _on_timer_timeout():
+	is_clicked = true
