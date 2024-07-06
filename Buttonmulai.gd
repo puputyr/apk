285b2d03dccc50041ef7cd0953af794556a90eae
Puputtt <puputsn9@gmail.com>
2024-07-06 23:10:48 +0700

    button mulai ke intri
    mbuh kok seng quiz tak add malah button mulai moro ng quiz SEKKKKKKK ERROR

--- Buttonmulai.gd
+++ Buttonmulai.gd
@@ -1,5 +1,8 @@
 extends Button
 
 func _input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		get_tree().change_scene("res://intro.tscn")
+	if (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT) or event is InputEventScreenTouch and event.pressed :
+		
+			get_tree().change_scene_to_file("res://intro.tscn")
+func _on_touch_screenmulai_pressed():
+	pass # Replace with function body.
