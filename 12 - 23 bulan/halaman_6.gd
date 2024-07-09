56305d5b9a0d5076ebeed8539525cdffc16f6ca4
HeigelMuhammad <heigeloptcrs@gmail.com>
2024-07-09 17:02:03 +0700

    6 -8 Bulan dan 12 - 23 Bulan
    Button berfungsi

---
+++ 12 - 23 bulan/halaman_6.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://12 - 23 bulan/halaman_7.tscn")
+	pass # Replace with function body.
