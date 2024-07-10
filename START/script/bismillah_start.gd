5f907f7b4e7945551d9b05f0c2ea0327602d2197
Zakaria Zidan Zamroni <161435839+ZidanParker@users.noreply.github.com>
2024-07-10 19:54:05 +0700

    update
    update lagu

--- START/script/bismillah_start.gd
+++ START/script/bismillah_start.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	AudioPlayer.play_music_level()
 	pass # Replace with function body.
 
 
@@ -11,10 +12,14 @@ func _process(delta):
 	pass
 
 func _on_touch_screenmulai_released():
-	get_tree().change_scene_to_file("res://scene/intro.tscn")
+	get_tree().change_scene_to_file("res://START/scene/intro.tscn")
 	pass # Replace with function body.
 
 
 func _on_touch_screenquiz_released():
 	get_tree().change_scene_to_file("res://modul_kuis/kuis.tscn")
 	pass # Replace with function body.
+
+
+func _on_buttonmulai_pressed():
+	get_tree().change_scene_to_file("res://START/scene/intro.tscn")
